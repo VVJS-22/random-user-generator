a411c7f813e05c4aa906dbf48daada11490eee39
VVJS-22 <s.jayeshchennai@gmail.com>
2021-11-17 19:06:01 +0530

    copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,70 +1,91 @@
-import tw from 'tailwind-styled-components'
-import { useEffect, useState } from 'react'
+import tw from "tailwind-styled-components";
+import { useEffect, useState } from "react";
+
+const colors = [
+  "pink",
+  "purple",
+  "indigo",
+  "blue",
+  "green",
+  "yellow",
+  "red",
+  "gray",
+];
 
 const Wrapper = tw.div`
-  bg-blue-800 h-full
+  h-full
   flex justify-center items-center
-`
+`;
 
 const Deck = tw.div`
   flex flex-col justify-center items-center
-`
+  z-10
+`;
 
 const Card = tw.div`
   max-w-sm
   min-h-60
   w-11/12
-  bg-blue-600
   rounded-lg
   shadow-xl
-`
+`;
 
 const Profile = tw.img`
   w-24 h-24 bg-white rounded-full
   absolute inset-x-1/2
   transform -translate-x-1/2 -translate-y-1/2
-`
+`;
 
 const Details = tw.div`
   mt-16 text-white px-12 py-5
   leading-loose
-`
+`;
 
 const Data = tw.div`
   cursor-pointer
-`
+`;
 
 const Span = tw.div`
   font-bold
   inline-block
   w-2/5
-`
+`;
 
 const Button = tw.button`
   bg-white px-6 py-2
   self-end
   transform translate-y-8
-  text-blue-600
   font-bold
   rounded-lg
+`;
+
+const Alert = tw.p`
+  bg-white
+  px-6
+  py-4
+  font-bold
+  text-4xl
+  fixed inset-0
 `
 
-const URL = 'https://randomuser.me/api/?results=1'
+const URL = "https://randomuser.me/api/?results=1";
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+  const [color, setColor] = useState("blue");
+  const [alert, setAlert] = useState(false)
 
   const usefetch = async () => {
-    const response = await fetch(URL)
-    const data = await response.json()
-    console.log(data.results[0])
-    const user = data.results[0]
-    const { cell, email, gender } = user
-    const { large: image } = user.picture
-    const { first, last} = user.name
-    const { date: dob } = user.dob
-    const { location: {city, state} } = user
-
+    const response = await fetch(URL);
+    const data = await response.json();
+    const user = data.results[0];
+    const { cell, email, gender } = user;
+    const { large: image } = user.picture;
+    const { first, last } = user.name;
+    const { date: dob } = user.dob;
+    const {
+      location: { city, state },
+    } = user;
 
     const newUser = {
       image,
@@ -74,46 +95,75 @@ const App = () => {
       city,
       state,
       cell,
-      email
-    }
-    setUser(newUser)
-
-    console.log(Object.entries(user))
-  }
-
+      email,
+    };
+    setUser(newUser);
+    setColor(colors[Math.floor(Math.random() * 8)]);
+  };
 
   useEffect(() => {
-    usefetch()
-  },[])
+    usefetch();
+  }, []);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    },500)
 
+    return () => clearTimeout(timeout)
+  },[alert])
 
   return (
-    <Wrapper>
+    <Wrapper className={`bg-${color}-800`}>
+      {alert && <Alert>Copied to clipboard</Alert>}
       <Deck>
-        <Card>
-          <Profile src={user.image} alt="Profile"/>
+        <Card className={`bg-${color}-600`}>
+          <Profile src={user && user.image} alt="Profile" />
           <Details>
-            {user && Object.entries(user).map((entry, index) => {
-              if (index !== 0) {
-                return(
-                  <Data key={index}>
-                    <Span>{entry[0]}</Span>: {entry[1]}
-                  </Data>
-                )
-              }
-              return null
-            })}
+            {user &&
+              Object.entries(user).map((entry, index) => {
+                if (index !== 0) {
+                  return (
+                    <Data
+                      key={index}
+                      title="Click to copy"
+                      onClick={() => {
+                        setAlert(true);
+                        navigator.clipboard.writeText(entry[1]);
+
+                      }}
+                    >
+                      <Span>
+                        {index === 2
+                          ? "D.o.B"
+                          : entry[0].slice(0, 1).toUpperCase() +
+                            entry[0].slice(1)}
+                      </Span>
+                      :{" "}
+                      {index === 2
+                        ? entry[1].slice(0, 10).split("-").reverse().join("-")
+                        : index === 7
+                        ? entry[1]
+                        : entry[1].slice(0, 1).toUpperCase() +
+                          entry[1].slice(1)}
+                    </Data>
+                  );
+                }
+                return null;
+              })}
           </Details>
         </Card>
         <Button
-        onClick={() => usefetch()}
-        >Next</Button>
+          className={`text-${color}-600`}
+          onClick={() => {
+            usefetch();
+          }}
+        >
+          Next
+        </Button>
       </Deck>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
